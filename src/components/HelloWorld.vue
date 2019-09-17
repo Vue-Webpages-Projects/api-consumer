c66ae7ef3f8f01114969ac9fd90a900a3876124c
lpgoulart <leonardo@roffor.com.br>
2019-09-17 13:48:23 -0300

    upload

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    {{info}}
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      info: null
+    }
+  },
+
+  created() {
+    axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => (this.info = response.data.bpi))
+      .catch(e => {
+      this.errors.push(e)
+    })
+
+  }
+}
+</script>
