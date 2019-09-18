fd134a4dffdb39d46225a412ac64e64ee3534778
lpgoulart <leonardo@roffor.com.br>
2019-09-18 11:41:51 -0300

    add currency value for bitcoin

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
-    {{info}}
+    <!-- {{info}} -->
+
+    <div 
+      v-for="currency in info"
+      v-bind:key="currency.code"
+      >
+      {{currency.description}}
+
+      <span class="lighten">
+        <span v-html="currency.symbol"></span>{{ currency.rate_float | currencydecimal }}
+      </span>
+    </div>
+
+    
+
+
   </div>
 </template>
 
@@ -21,7 +36,11 @@ export default {
       .catch(e => {
       this.errors.push(e)
     })
-
+  },
+  filters: {
+    currencydecimal ( value ) {
+      return value.toFixed(2)
+    }
   }
 }
 </script>
